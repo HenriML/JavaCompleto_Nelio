99fb35c8ebc78667ebe40ea1688c3b4c1506c47c
Henrique Munhoz Leite <88989379+HenriML@users.noreply.github.com>
2025-02-25 17:34:13 -0300

    Feat: Adjusting enviroment

---
+++ src/edu/henrique/Main.java
@@ -0,0 +1,16 @@
+package edu.henrique;
+
+public class Main {
+    public static void main(String[] args) {
+        //ProdutoInfo.executar();
+        System.out.println("AAAAAAA");
+        //Operacoes.somando();
+        //Operacoes.exemplo1();
+        //Operacoes.exemplo2();
+        //Operacoes.exemplo3();
+        //Operacoes.exemplo4();
+        //Operacoes.exemplo5();
+    }
+}
+
+// Aula mais recente: #A021
