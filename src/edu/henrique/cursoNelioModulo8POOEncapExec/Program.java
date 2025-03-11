36ad8c99285f9993a10c3f23b36dc8c8ef442b62
Henrique Munhoz Leite <88989379+HenriML@users.noreply.github.com>
2025-03-11 17:33:23 -0300

    Chore: Comment fix

--- src/edu/henrique/cursoNelioModulo8POOEncapExec/Program.java
+++ src/edu/henrique/cursoNelioModulo8POOEncapExec/Program.java
@@ -26,47 +26,49 @@ public class Program {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Account account;
+        Account account; // Declaração da variável `account` para armazenar a instância da classe `Account`.
+
 
 
         System.out.print("Welcome to the bankRupcy!\n ");
         System.out.print("Enter account number: ");
-        int accountNumber = sc.nextInt();
+        int accountNumber = sc.nextInt(); //Declaro essa var de forma auxiliar para captar o valor do user e inserir na var da account
         System.out.print("Enter account name: ");
-        sc.nextLine();
+        sc.nextLine(); //                                              ||
         String accountHolder = sc.nextLine();
         System.out.print("Is there an initial deposit (y/n)? ");
-        char response = sc.next().charAt(0);
+        char response = sc.next().charAt(0); //Declaro essa para validar se há depósito ou não deposito inicial
         if (response == 'y' || response == 'Y') {
             System.out.print("Enter initial deposit value: ");
-            double InitialDeposit = sc.nextDouble();
-            account = new Account(accountNumber, accountHolder, InitialDeposit);
+            double InitialDeposit = sc.nextDouble(); // Mesma ideia de captar o valor com uma var auxiliar de mesmo nome, afinal só account foi declarada, e não as var dentro dela
+            account = new Account(accountNumber, accountHolder, InitialDeposit); //Caso positivo, inicio com três parâmetros, sendo um deles o valor capturado do user
         }else {
-            account = new Account(accountNumber, accountHolder);
+            account = new Account(accountNumber, accountHolder);// Caso negativo, só instancio com dois, deixando o valor padrão de 0 para InitialDeposit
         }
         System.out.println();
 
         System.out.println();
         System.out.println("Account data: \n");
-        System.out.println(account);
+        System.out.println(account); // Exibe os dados da conta usando o método `toString` da classe `Account`.
+
 
 
         System.out.println();
         System.out.println("Enter a deposit value: ");
-        double depositValue = sc.nextDouble();
-        account.deposit(depositValue);
+        double depositValue = sc.nextDouble(); //Coleto o valor com a var auxiliar do depósito
+        account.deposit(depositValue); // Jogo dentro da var constante account o valor coletado em depositValue como parãmetro, usando a função deposit que com as vars auxiliares, coletaram o deposit, jogaram o valor em amount e somaram com o initialDeposit
         System.out.print("Updated account data: ");
-        System.out.println(account);
+        System.out.println(account); // Mostro o valor atualizado
 
         System.out.println();
         System.out.println("Enter a deposit value: ");
-        double withdrawValue = sc.nextDouble();
-        account.withdraw(withdrawValue);
+        double withdrawValue = sc.nextDouble(); //Tal qual acima, porém com o valor invertido, retirando da conta
+        account.withdraw(withdrawValue); //Mesmo processo acima, porém com sinal invertido de retirada + acrécimo de 5 negativo da "taxa bancária"
         System.out.print("Updated account data: ");
-        System.out.println(account);
+        System.out.println(account); // Mostro o valor atualizado
 
 
 
-        sc.close();
+        sc.close();// Finalizo o código
     }
 }
