d77c8e635507fa6f6ce1228bcff57e80bef8d16e
Henrique Munhoz Leite <88989379+HenriML@users.noreply.github.com>
2025-03-17 20:39:19 -0300

    Update Program.java

--- src/edu/henrique/cursoNelioParte2/cursoNelioModulo11List/Program.java
+++ src/edu/henrique/cursoNelioParte2/cursoNelioModulo11List/Program.java
@@ -1,5 +1,9 @@
 package edu.henrique.cursoNelioParte2.cursoNelioModulo11List;
 
 public class Program {
-    
+    public static void application(){
+        System.out.println("ATA");
+    }
+
+
 }
