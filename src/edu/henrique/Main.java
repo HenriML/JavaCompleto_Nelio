6287f985236bb79695f9ceea4fbb490bed37337f
Henrique Munhoz Leite <88989379+HenriML@users.noreply.github.com>
2025-03-25 14:44:05 -0300

    Feat: Heritage

--- src/edu/henrique/Main.java
+++ src/edu/henrique/Main.java
@@ -1,6 +1,6 @@
 package edu.henrique;
 
-import edu.henrique.cursoNelioParte2.cursoNelioModulo14.*;
+import edu.henrique.cursoNelioParte2.cursoNelioModulo15.Program;
 
 import java.text.ParseException;
 
@@ -51,6 +51,7 @@ public class Main {
         //datecalc.application();
         //-------------------------------------A062 - A084 Abaixo - POO --------------------------------------------------
 
+        //Program.application();
         Program.application();
 
 
@@ -58,5 +59,5 @@ public class Main {
 
 }
 
-// Aula para assistir: #A131 (Começo)
+// Aula para assistir: #A136 (Começo)
 // Alterar rotina
---
+++ src/edu/henrique/cursoNelioParte2/cursoNelioModulo15/Program.java
@@ -0,0 +1,14 @@
+package edu.henrique.cursoNelioParte2.cursoNelioModulo15;
+
+import edu.henrique.cursoNelioParte2.cursoNelioModulo15.Entities.BusinessAccount;
+
+public class Program {
+    public static void application() {
+
+        BusinessAccount account = new BusinessAccount();
+
+        account.getBalance(); // Usando o get da account
+        
+
+    }
+}
