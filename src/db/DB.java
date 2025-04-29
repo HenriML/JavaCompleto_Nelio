43d8658b376c8bc501a11c2691e6461fb3e036fe
Henrique Munhoz Leite <88989379+HenriML@users.noreply.github.com>
2025-04-29 15:20:22 -0300

    Feat: Collecting data from MySQL and displaying in the console

--- src/db/DB.java
+++ src/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 
@@ -45,6 +43,26 @@ public class DB {
         }
         catch(IOException e){
             throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeStatement(Statement st){ // Fechamento do statement, assim evito TryCatch no código inteiro
+        if (st != null){
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs){ // Fechamento do statement, assim evito TryCatch no código inteiro
+        if (rs != null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
         }
     }
 }
---
+++ src/edu/henrique/cursoNelioParte3/cursoNelioModulo32/Program.java
@@ -0,0 +1,48 @@
+package edu.henrique.cursoNelioParte3.cursoNelioModulo32;
+
+
+
+// JDBC - Java DataBase Connectivity (API padrão do JAVA para DB)
+
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program {
+    public static void application(){
+
+
+        Connection conn = null; // Prepara a conexão com o banco
+        Statement st = null;  // Faz a consulta dos dados
+        ResultSet rs = null;  // Recebe os dados e armazena nela mesma
+
+        try{
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+
+            rs = st.executeQuery("SELECT * FROM department"); // O executeQuery deixa executar uma query no parametro
+
+            while(rs.next()){
+                System.out.println(rs.getInt("Id") +"," + rs.getString("Name")); // Usando o rs.getAlgo você pdoe coletar diretamente os campos, e como tem um next de parametro, ele vai caapturar até o próximo ser nulo
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally { // Como esses métodos não são chamados via JAVA JDK(Interno) se não fechar pode vazar memória e estourar, então o fuinally serve para isso, além de quê para evitar try/catch (Statements podem dar) eu tratei deles lá no DBClass
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+            DB.closeConnection();
+        }
+
+
+
+
+
+    }
+}
